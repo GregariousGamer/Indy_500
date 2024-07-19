12295f76c952fda254608d4f192ead58928d016d
Gregarious Gamer <gregariousgamer0.gmail.com>
2024-07-19 10:45:54 -0400

    playing with car physics

--- entities/player/car.gd
+++ entities/player/car.gd
@@ -2,18 +2,19 @@ extends CharacterBody2D
 
 const acceleration: float = 1.0
 
-var speed: float = 10.0
-var rotation_speed: float = 2.5
-var friction: float = acceleration / speed * 4.0
-var move_input: float
-var rotation_direction: float
-var max_speed: float = 750.0
+@export var speed: float = 10.0
+@export var rotation_speed: float = 2.0
+@export var friction: float = acceleration / speed * 10.0
+@export var move_input: float
+@export var rotation_direction: float
+@export var max_speed: float = 1000.0
+@export var turn_speed_minimum: float = 150.0
 
 func _process(delta: float) -> void:
 	# seperate from physics process
 	apply_traction(delta)
 	apply_friction(delta)
-
+ 
 func _physics_process(delta: float) -> void:
 	move_input = Input.get_axis("ui_up", "ui_down")
 	rotation_direction = Input.get_axis("ui_left", "ui_right")
@@ -47,7 +48,8 @@ func apply_traction(delta: float) -> void:
 
 	velocity += transform.x * move_input * speed * acceleration # includes direction and move input and acceleration
 	if velocity != Vector2.ZERO: # stops the car from rotating if not moving
-		rotation += rotation_direction * rotation_speed * delta
+		if abs(velocity.y) || abs(velocity.x) >= turn_speed_minimum: # minimum speed required to turn effectively
+			rotation += rotation_direction * rotation_speed * delta
 
 # called every tick to slow the car down if not moving	
 func apply_friction(delta: float) -> void:
@@ -55,4 +57,4 @@ func apply_friction(delta: float) -> void:
 
 # called when right or left are held down, reduces speed for more realistic driving	
 func apply_turn_friction(delta: float) -> void:
-	velocity -= velocity/2 * friction * delta
+	velocity -= (velocity * friction * delta) / 250
