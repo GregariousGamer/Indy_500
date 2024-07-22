e1a4c0d507b724e8c96045d35f333b61997da31e
Gregarious Gamer <gregariousgamer0.gmail.com>
2024-07-22 15:34:06 -0400

    done with car for now

--- entities/player/car.gd
+++ entities/player/car.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 const acceleration: float = 1.0
 
-@export var speed: float = 5.0
+@export var speed: float = 3.0
 @export var rotation_speed: float = 2.25
-@export var friction: float = acceleration / speed * 5.0
+@export var friction: float = acceleration / speed * 1.5
 @export var move_input: float
 @export var rotation_direction: float
 @export var max_speed: float = 750.0
@@ -16,7 +16,7 @@ func _ready() -> void:
 	self.scale *= 0.5
 
 func _process(delta: float) -> void:
-	# car particles !!NEEDS WORK
+	# car particles, based on direction of car !!NEEDS WORK
 	if self.velocity.y < 0:
 		car_trails.position = self.position + Vector2(0, 5)
 	if self.velocity.y > 0:
@@ -27,6 +27,9 @@ func _process(delta: float) -> void:
 	if (self.velocity.y == 0
 	&& self.velocity.x > 0):
 		car_trails.position = self.position + Vector2(3, 8)
+	if (self.velocity.y == 0
+	&& self.velocity.x == 0):
+		car_trails.position = self.position + Vector2(3, 5)
 	# seperate from physics process
 	apply_traction(delta)
 	apply_friction(delta)
@@ -64,8 +67,8 @@ func apply_traction(delta: float) -> void:
 
 	velocity += transform.x * move_input * speed * acceleration # includes direction and move input and acceleration
 	if velocity != Vector2.ZERO: # stops the car from rotating if not moving
-		if abs(velocity.y) || abs(velocity.x) >= turn_speed_minimum: # minimum speed required to turn effectively
-			rotation += rotation_direction * rotation_speed * delta
+		#if abs(velocity.y) || abs(velocity.x) >= turn_speed_minimum: # minimum speed required to turn effectively
+		rotation += rotation_direction * rotation_speed * delta
 
 # called every tick to slow the car down if not moving	
 func apply_friction(delta: float) -> void:
@@ -73,4 +76,4 @@ func apply_friction(delta: float) -> void:
 
 # called when right or left are held down, reduces speed for more realistic driving	
 func apply_turn_friction(delta: float) -> void:
-	velocity -= (velocity * friction * delta)
+	velocity -= (velocity * friction * delta) * 1.25
